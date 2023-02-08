d1cb1688451c39f5b0f259bf069a2213e1221821
Young Koh <youngheon6@gmail.com>
2023-02-08 10:16:35 -0700

    add methods/tests for dealership class

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -4,6 +4,7 @@ class Dealership
     @name = name
     @address = address
     @inventory = []
+    @total_value = 0
   end
 
   def inventory_count
@@ -12,5 +13,36 @@ class Dealership
 
   def add_car(car)
     @inventory << car
+  end
+
+  def has_inventory?
+    if @inventory.length > 0
+      true
+    else
+      false
+    end
+  end
+
+  def cars_by_make(make)
+    same_make_cars = []
+    @inventory.each do |car|
+      if car.make == make
+        same_make_cars << car
+      end
+    end
+    same_make_cars
+  end
+
+  def total_value
+    @inventory.each do |car|
+      @total_value += car.total_cost
+    end
+    @total_value
+  end
+
+  def details
+    self.total_value
+    details_hash = {"total_value" => @total_value, "address" => @address}
+    require 'pry'; binding.pry
   end
 end
