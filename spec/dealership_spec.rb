2b68fd398fbf023193229be1209a4e46f00c2c35
Young Koh <youngheon6@gmail.com>
2023-02-08 09:40:49 -0700

    Update car/dealership class and tests

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/car'
+require './lib/dealership'
+
+RSpec.describe Car do
+  it "exists" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership).to be_a Dealership
+  end
+
+  it "has a name" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.name).to eq("Acme Auto")
+  end
+
+  it "has an address" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.address).to eq("123 Main Street")
+  end
+end
