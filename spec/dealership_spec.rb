0ff768fc9f5ee1640a1606e64f95539abecad614
Young Koh <youngheon6@gmail.com>
2023-02-08 09:46:10 -0700

    update dealership class and tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe Car do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
     expect(dealership.address).to eq("123 Main Street")
   end
+
+  it "can count the inventory" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    expect(dealership.inventory_count).to eq(0)
+  end
+
+  it "can add cars to inventory" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    expect(dealership.inventory_count).to eq(2)
+    require 'pry'; binding.pry
+  end
+
+  
 end
